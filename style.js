d8126174bb842458726213603fb2917a5b0bcbe9
LukeGeyer1117 <luke.geyer@utahtech.edu>
2025-02-05 13:41:47 -0700

    made a final fix

--- style.js
+++ style.js
@@ -4,19 +4,19 @@ let dropdownHolder = document.querySelector("#dropdown-holder");
 let dropdownArrow = document.querySelector("#dropdown-holder img");
 
 dropdownHolder.addEventListener("mouseover", function() {
-    dropdownArrow.src = `${window.location.origin}/images/dropdown_arrow.svg`;
+    dropdownArrow.src = `${window.location.origin}/portfolio/images/dropdown_arrow.svg`;
 });
 
 dropdownHolder.addEventListener("mouseout", function () {
-    dropdownArrow.src = `${window.location.origin}/images/dropup_arrow.svg`;
+    dropdownArrow.src = `${window.location.origin}/portfolio/images/dropup_arrow.svg`;
 });
 
 // Keep the dropdown arrow if the user hovers over the dropdown-content
 let dropdown = document.querySelector(".dropdown");
 dropdown.addEventListener("mouseover", function () {
-    dropdownArrow.src = `${window.location.origin}/images/dropdown_arrow.svg`;
+    dropdownArrow.src = `${window.location.origin}/portfolio/images/dropdown_arrow.svg`;
 });
 
 dropdown.addEventListener("mouseout", function () {
-    dropdownArrow.src = `${window.location.origin}/images/dropup_arrow.svg`;
+    dropdownArrow.src = `${window.location.origin}/portfolio/images/dropup_arrow.svg`;
 });
